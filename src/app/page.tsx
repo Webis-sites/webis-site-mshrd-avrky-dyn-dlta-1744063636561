7da322c1ef16a668c3a3db67da4834c1912aa695
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 01:34:28 +0300

    Add Layout component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import MetaTags from '../components/MetaTags';
+import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <main className="flex-1 container mx-auto px-4 py-8">
         {/* כאן יתווספו הקומפוננטות שייווצרו על ידי המחולל */}
         <MetaTags />
+    <Layout />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
